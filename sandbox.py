ae6f36e1aefe4a6474ba58e081ace82e69240e45
Aleksas Pielikis <ant.kampo@gmail.com>
2020-07-22 22:43:19 +0300

    Replace extraction with in memory iteration.

--- sandbox.py
+++ sandbox.py
@@ -1,15 +1,13 @@
 from vdu_nlp_services import stress_text
 from zipfile import ZipFile
-from os.path import isdir
-from shutil import rmtree
+from conllu import parse_incr
+from io import TextIOWrapper
 import re
 
 matas_archive_filename = './dataset/MATAS-v1.0.zip'
-matas_target_directory = './dataset/'
-matas_extracted_directory = matas_target_directory + '/MATAS-v1.0'
 
-pattern = re.compile(r'(\d+)\.\s+([^\(]+)\s*\(([^\)]+)\)')
-
+stress_pattern = re.compile(r'(\d+)\.\s+([^\(]+)\s*\(([^\)]+)\)')
+matas_conllu_filename_pattern = re.compile(r'MATAS-v1\.0\/CONLLU\/.*\.conllu')
 
 def stress(word):
 	res = stress_text(word.strip()).splitlines()
@@ -18,24 +16,25 @@ def stress(word):
 		yield {'word': res[0].strip(), 'details': []}
 	else:
 		for line in res:
-			m = pattern.match(line)
+			m = stress_pattern.match(line)
 			if m:
 				yield {'word': m.group(2), 'details': m.group(3).split(' ')}
 			else:
 				raise Exception()
 
-def unpack(force=False):
-	dir_exists = isdir(matas_extracted_directory)
-	if force and dir_exists:
-		rmtree(matas_extracted_directory)
-		dir_exists = False
-
-	if not dir_exists:
-		with ZipFile(matas_archive_filename, 'r') as zip_ref:
-			zip_ref.extractall(matas_target_directory)
-
-unpack()
+def dataset_connlu_files(encoding='utf-8'):
+	with ZipFile(matas_archive_filename, 'r') as zip_ref:
+		for filename in zip_ref.namelist():
+			if matas_conllu_filename_pattern.match(filename):
+				with zip_ref.open(filename, 'r') as fp:
+					with TextIOWrapper(fp, encoding=encoding) as text_fp:
+						yield text_fp
 
 res = stress('keliai')
 for r in res:
 	print (r)
+
+
+for fp in dataset_connlu_files():
+	for tokenlist in parse_incr(fp):
+		print(tokenlist)
