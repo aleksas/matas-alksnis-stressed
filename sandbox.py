039dfd8e184d9cf128a57ea0e02b8777b1d14378
Aleksas Pielikis <ant.kampo@gmail.com>
2020-07-22 23:42:13 +0300

    Iterate and stress. Add paragraph and document handling.

--- sandbox.py
+++ sandbox.py
@@ -6,23 +6,22 @@ import re
 
 matas_archive_filename = './dataset/MATAS-v1.0.zip'
 
-stress_pattern = re.compile(r'(\d+)\.\s+([^\(]+)\s*\(([^\)]+)\)')
+stress_pattern = re.compile(r'(\d+\.)\s+([^\(]+)\s+\(([^\)]+)\)')
 matas_conllu_filename_pattern = re.compile(r'MATAS-v1\.0\/CONLLU\/.*\.conllu')
 
 def stress(word):
 	res = stress_text(word.strip()).splitlines()
 
-	if len(res) == 1:
-		yield {'word': res[0].strip(), 'details': []}
-	else:
-		for line in res:
-			m = stress_pattern.match(line)
-			if m:
-				yield {'word': m.group(2), 'details': m.group(3).split(' ')}
-			else:
-				raise Exception()
+	for line in res:
+		m = stress_pattern.match(line)
+		if m:
+			word = m.group(2)
+			details = m.group(3).split(' ')
+			yield {'word': word, 'details': details}
+		else:
+			yield {'word': word, 'details': []}
 
-def dataset_connlu_files(encoding='utf-8'):
+def get_dataset_connlu_files(encoding='utf-8'):
 	with ZipFile(matas_archive_filename, 'r') as zip_ref:
 		for filename in zip_ref.namelist():
 			if matas_conllu_filename_pattern.match(filename):
@@ -30,11 +29,33 @@ def dataset_connlu_files(encoding='utf-8'):
 					with TextIOWrapper(fp, encoding=encoding) as text_fp:
 						yield text_fp
 
-res = stress('keliai')
-for r in res:
-	print (r)
+def get_stessed_sentences():
+	for fp in get_dataset_connlu_files():
+		for tokenlist in parse_incr(fp):
+			offset = 0
+			sentence = tokenlist.metadata['text']
+			stressed_sentence = ''
+			
+			for token in tokenlist:
+				tags = token['xpos']
+				word = token['form']
 
+				word_offset = sentence.find(word, offset)
+				stressed_sentence += sentence[offset: word_offset]
 
-for fp in dataset_connlu_files():
-	for tokenlist in parse_incr(fp):
-		print(tokenlist)
+				offset = word_offset + len(word)
+
+				for each in stress(word):
+					stressed_sentence += each['word']
+					# TODO: add mapping VDU stress to MATAS tags
+					# subtract setts, stress with smallest set size should be selected
+					break
+			
+			stressed_sentence += sentence[offset:]
+
+			yield sentence, stressed_sentence #, (tokenlist.metadata['sent_id'], tokenlist.metadata['newpar id'], tokenlist.metadata['newdoc id'])
+
+for sentence, stressed_sentence in get_stessed_sentences():
+	print(sentence)
+	print(stressed_sentence)
+	print()
