bec63d52ec78734d8bc8b6d14ca5f4822a0c8e29
Aleksas Pielikis <ant.kampo@gmail.com>
2020-07-31 10:26:57 +0300

    Update sentences.py

--- sentences.py
+++ sentences.py
@@ -4,12 +4,12 @@ from io import TextIOWrapper
 import re
 
 datasets = [
-	('./datasets/Alksnis-3.0.zip', re.compile(r'Alksnis-3.0\/.*\.conllu')),
-	('./datasets/MATAS-v1.0.zip', re.compile(r'MATAS-v1\.0\/CONLLU\/.*\.conllu'))
+	('./datasets/Alksnis-3.0.zip', re.compile(r'Alksnis-3.0\/.*\.conllu'), 'utf-8'),
+	('./datasets/MATAS-v1.0.zip', re.compile(r'MATAS-v1\.0\/CONLLU\/.*\.conllu'), 'utf-8')
 ]
 
-def get_dataset_connlu_files(encoding='utf-8'):
-	for archive_filename, conllu_filename_pattern in datasets:
+def get_dataset_connlu_files():
+	for archive_filename, conllu_filename_pattern, encoding in datasets:
 		with ZipFile(archive_filename, 'r') as zip_ref:
 			for filename in zip_ref.namelist():
 				if conllu_filename_pattern.match(filename):
@@ -17,8 +17,8 @@ def get_dataset_connlu_files(encoding='utf-8'):
 						with TextIOWrapper(fp, encoding=encoding) as text_fp:
 							yield text_fp
 
-def get_sentences(encoding='utf-8'):
-	for fp in get_dataset_connlu_files(encoding=encoding):
+def get_sentences():
+	for fp in get_dataset_connlu_files():
 		for tokenlist in parse_incr(fp):
 			yield tokenlist.metadata['text']
 
